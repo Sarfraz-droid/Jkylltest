811331109c2ee0e8b5dd7e0810e16af3ad79ee9d
Sarfraz <alamsarfraz422@gmail.com>
2022-06-06 12:08:12 +0530

    Init

---
+++ blogs/Blog.md
@@ -0,0 +1,7 @@
+# Sample Blog
+
+Est fugiat sint exercitation non. Adipisicing cupidatat sint est exercitation culpa qui sunt ea excepteur non commodo duis quis dolore. Aliqua eiusmod veniam laborum dolore fugiat mollit et minim occaecat quis esse consequat cupidatat.
+
+Nisi nisi et deserunt eiusmod ullamco labore exercitation minim ad fugiat ea. Magna laborum do ad officia est laboris dolore eu adipisicing duis sunt. Lorem laborum consectetur est duis nostrud velit occaecat eu.
+
+Reprehenderit adipisicing labore esse consectetur id qui voluptate nostrud mollit commodo. Id nostrud do et eu. Deserunt ullamco voluptate eu veniam sint occaecat do cillum aute eu excepteur nostrud exercitation. Lorem officia culpa laborum est deserunt nulla id eu laborum nulla magna. Laboris quis ex tempor magna officia occaecat sit proident eu occaecat velit nostrud voluptate laborum. Dolor esse Lorem exercitation adipisicing velit ex culpa fugiat dolore quis id.
---
+++ index.js
@@ -0,0 +1,61 @@
+import fs from 'fs'
+import path,{dirname} from 'path'
+
+const unusedFolders = [".git", "node_modules", "public", "src", "test"]
+
+const parseMarkdown = (filePath) => { 
+    const file = fs.readFileSync(filePath, 'utf8')
+    const lines = file.split('\n')
+    const title = lines[0].replace('#', '').trim()
+    const link = filePath;
+
+    return {
+        title,
+        path : link
+    }
+}
+
+const parseFolders = (folderPath) => { 
+    console.log(folderPath)
+    const folderpaths = {
+        items: [],
+        prefixes: []
+    };
+    const files = fs.readdirSync(folderPath)
+    files.forEach(file => {
+        const filePath = path.join(folderPath, file)
+        const stat = fs.statSync(filePath)
+        if (stat.isDirectory()) {
+            if (unusedFolders.indexOf(file) === -1) {
+                folderpaths.prefixes.push(parseFolders(filePath))
+            }
+        } else {
+            if (file.indexOf('.md') > -1) {
+                folderpaths.items.push(parseMarkdown(filePath))
+            }
+        }
+    })
+
+
+    console.log(folderpaths)
+
+    return folderpaths;
+}
+
+const folderpaths = {};
+
+fs.readdirSync(dirname(''), { withFileTypes: true }).forEach(file => { 
+
+    if (file.isDirectory() && !unusedFolders.includes(file.name)) {
+        folderpaths[file.name] = {
+            path: `/${file.name}`,
+            name: file.name,
+            chilren: parseFolders(`${file.name}`)
+        }
+    }
+})
+
+fs.writeFile('./index.json', JSON.stringify(folderpaths), (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+});
